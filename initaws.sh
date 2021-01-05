45e4aa6b2e63d8429a2b145485cb00d43610e32c
Waldemar Hummer <waldemar.hummer@gmail.com>
2021-01-05 19:52:57 +0100

    minor

--- initaws.sh
+++ initaws.sh
@@ -1,2 +1,4 @@
 
 awslocal elasticache create-cache-cluster --cache-cluster-id c1
+
+awslocal rds create-db-instance --db-instance-identifier db1 --db-instance-class c1 --engine postgres --master-username obscure-user --master-user-password obscure-password
--- src/app.py
+++ src/app.py
@@ -37,7 +37,8 @@ class Application:
         self.db.init_app(self.flask_app)
 
         # Init Flask-Migrate
-        u.wait_for_service('postgres', 5432, timeout=30.0)
+        # u.wait_for_service('postgres', 5432, timeout=30.0)
+        u.wait_for_service('localstack', 5414, timeout=30.0)
         self.migrate = Migrate(self.flask_app, self.db)
 
     def init_routes(self):
